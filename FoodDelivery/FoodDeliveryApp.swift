640045b8571d60fa3962a6e6f2eb1e386062886b
Alvina Medav <alvinamed25@gmail.com>
2025-01-27 12:32:50 +0530

    Initial Commit

---
+++ FoodDelivery/FoodDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryApp.swift
+//  FoodDelivery
+//
+//  Created by alvina medav  on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
