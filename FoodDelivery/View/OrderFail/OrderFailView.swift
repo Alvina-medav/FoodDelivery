a728f627e96fbf69851073aaafbba21a8010007f
Alvina Medav <alvinamed25@gmail.com>
2025-02-21 00:12:27 +0530

    commit changes

---
+++ FoodDelivery/View/OrderFail/OrderFailView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderFailView.swift
+//  FoodDelivery
+//
+//  Created by alvina medav  on 2/20/25.
+//
+
+import SwiftUI
+
+struct OrderFailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderFailView()
+}
